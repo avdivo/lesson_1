1143e5ff825b2105bd1f50721a25f84091fb23cc
avdivo <avdivo@mail.ru>
2021-05-24 21:24:11 +0300

    второй вариант решения

--- my_tasks/3_words.py
+++ my_tasks/3_words.py
@@ -7,8 +7,8 @@
 "mother bill mother" и т.д. 
 Тут правильным ответом должно быть "abba com mother" (частота — 3 раза).
 """
-import sys, os
-
+import sys, os, copy
+from collections import Counter
 
 all_words = []
 with open(os.path.join(sys.path[0], '3_words.txt')) as f: # Читаем все слова из файла из всех строк и заносим их в список
@@ -21,7 +21,9 @@ if len_all_words < 1: sys.exit("В файле мало слов!")
 for index in  range(len_all_words):
     three_words_sort = [all_words[index], all_words[index + 1], all_words[index + 2]]
     three_words_sort.sort() # Сортируем списки из 3 слов для их дальнейшего сравнения
-    three_words.append(three_words_sort) # Добавляем списки по 3 в общий список
+    three_words.append(tuple(three_words_sort)) # Добавляем списки по 3 в общий список
+
+three_words_2 = copy.deepcopy(three_words) # Копия списка для реализации решения методом 2
 
 challenger = [] # 3 слова претенденты
 quantity = 0 # Количество повторов 3 слов
@@ -35,3 +37,11 @@ for index in  range(len_all_words):
         quantity = count + 1
 
 print ('Слова {} встречаются рядом в файле {} раз'.format(challenger, quantity))
+
+
+# 2 вариант решения
+counts = Counter(three_words_2) 
+counts = counts.most_common() # список с тройками слов и количествои
+
+print ('Слова {} встречаются рядом в файле {} раз'.format(counts[0][0], counts[0][1]))
+print (counts)
