f37bb386bcd4ceb9c3bae2d9eb2a0f6a469a9831
avdivo <avdivo@mail.ru>
2021-06-15 00:38:37 +0300

    Много новых задач

---
+++ lesson_1/new.py
@@ -0,0 +1,8 @@
+# Заменить все двоеточия (:) на точку с запятой (;) Подсчитать количество замен
+
+array = ['q', 'f', '2:r:', 'u', '8', '4', 'g', ':6', 'a', 'a', '5:6']
+cou = ''.join(array).count(':')
+array = list(map(lambda x: x if x.find(':') == -1 else x.replace(':', ';'), array))
+print (cou)
+print (array)
+
