1e56b67f822c781d7dbc21077f6316ba20c6056e
avdivo <avdivo@mail.ru>
2022-06-18 13:50:55 +0300

    sort_merge_sobes

--- my_tasks/sort_merge_sobes.py
+++ my_tasks/sort_merge_sobes.py
@@ -1,12 +1,6 @@
-
-
-
-
 def Merge(A, p, q, r):
-    if p == q:
-        return
     i = p - 1
-    j = q - 1
+    j = q
     n = []
     while i < q and j < r:
         if A[i] < A[j]:
@@ -16,7 +10,7 @@ def Merge(A, p, q, r):
             n.append(A[j])
             j += 1
     n += A[i:q] + A[j:r]
-    A[p:r] = n
+    A[p - 1:r] = n
 
 
 def Sort(A, p, r):
@@ -26,7 +20,22 @@ def Sort(A, p, r):
         Sort(A, q + 1, r)
         Merge(A, p, q, r)
 
-# A = [2, 6, 7, 3, 2, 9, 1, 0, 6, 3]
-A = [9, 2]
+A = []
 Sort(A, 1, len(A))
-print(A)
+assert A == [], 'Ошибка сортировки []'
+
+A = [1]
+Sort(A, 1, len(A))
+assert A == [1], 'Ошибка сортировки [1]'
+
+A = [2, 1]
+Sort(A, 1, len(A))
+assert A == [1, 2], 'Ошибка сортировки [2, 1]'
+
+A = [2, 1, 3]
+Sort(A, 1, len(A))
+assert A == [1, 2, 3], 'Ошибка сортировки [2, 1, 3]'
+
+A = [2, 1, 3, 6, 0, 2]
+Sort(A, 1, len(A))
+assert A == [0, 1, 2, 2, 3, 6], 'Ошибка сортировки [2, 1, 3, 6, 0, 2]'
